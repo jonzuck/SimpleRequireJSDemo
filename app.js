3599092f00c7809fdf95112225867a8bc10662f7
Jon Zuck <jonzuck@users.noreply.github.com>
2018-09-04 15:52:20 -0400

    corrected filename in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 define(['button'], function(button) {
     var btn = document.getElementById('theButton');
-    console.log('loaded myscript');
+    console.log('loaded app');
 
     btn.addEventListener('click', function() {
         console.log('Button clicked.');
